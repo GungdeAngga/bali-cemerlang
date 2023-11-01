ab714d3ca3406ff9f76dded986c754e334801234
Angga Aditya <agungangga2001@gmail.com>
2023-11-01 16:49:08 +0800

    modif dikit

--- src/component/searchTour/SearchFlightInput.js
+++ src/component/searchTour/SearchFlightInput.js
@@ -4,8 +4,12 @@ export default function SearchFlightInput() {
   return (
     <div class='flex items-center justify-center h-60'>
         <div class='w-11/12 h-16 bg-white'>
-            <div >
-                
+            <div class='grid grid-cols-5 divide-x'>
+            <div class='h-16'>01</div>
+            <div class='h-16'>02</div>
+            <div class='h-16'>03</div>
+            <div class='h-16'>04</div>
+            <div class='h-16'>05</div>
             </div>
         </div>
     </div>
