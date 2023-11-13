c457cfa9cf1f7fbf9e40c915b82269e163d38000
Angga Aditya <agungangga2001@gmail.com>
2023-11-13 17:37:10 +0800

    change navbar shrink position

--- src/component/navbar/Navbar.js
+++ src/component/navbar/Navbar.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ShrinkingNavbar from './ShrinkingNavbar';
 
 const Navbar = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+  
+    useEffect(() => {
+      window.addEventListener('scroll', () => {
+        setIsScrolled(window.scrollY > 0);
+      });
+  
+      // Clean up the event listener when the component unmounts
+      return () => {
+        window.removeEventListener('scroll', () => {
+          setIsScrolled(window.scrollY > 0);
+        });
+      };
+    }, []);
+
   return (
-    <div class='w-full'>
+    <div
+    className={`fixed top-0 left-0 w-full transition-all duration-300 ${
+      isScrolled ? 'h-24 bg-VividRed' : 'h-24 bg-transparent'
+    }`}
+    >
       <ShrinkingNavbar/>
       </div>
   );
--- src/component/navbar/ShrinkingNavbar.js
+++ src/component/navbar/ShrinkingNavbar.js
@@ -1,30 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import SearchImg from './SearchImg';
 import UserMenu from './UserMenu';
 import { Link } from 'react-router-dom';
 
 const ShrinkingNavbar = () => {
-    const [isScrolled, setIsScrolled] = useState(false);
-  
-    useEffect(() => {
-      window.addEventListener('scroll', () => {
-        setIsScrolled(window.scrollY > 0);
-      });
-  
-      // Clean up the event listener when the component unmounts
-      return () => {
-        window.removeEventListener('scroll', () => {
-          setIsScrolled(window.scrollY > 0);
-        });
-      };
-    }, []);
-
+    
   return (
-    <div
-    className={`fixed top-0 left-0 w-full transition-all duration-300 ${
-      isScrolled ? 'h-24 bg-VividRed' : 'h-24 bg-transparent'
-    }`}
-    >
       <div class='flex'>
         <div class='flex-relative p-4 pl-28'>
           <Link to='/'>
@@ -74,8 +55,6 @@ const ShrinkingNavbar = () => {
           </div>
         </div>
       </div>
-
-  </div>
   )
 }
 
