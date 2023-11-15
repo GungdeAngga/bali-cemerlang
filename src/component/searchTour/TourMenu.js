3723058c94e747c0f2e647e9967d437fd89c4bb3
Angga Aditya <theshadow1325@gmail.com>
2023-11-15 10:59:05 +0800

    fix text

--- src/component/searchTour/TourMenu.js
+++ src/component/searchTour/TourMenu.js
@@ -6,7 +6,11 @@ import SearchButtonTour from './SearchButtonTour'
 export default function TourMenu() {
   return (
     <div>
-
+        <div className="absolute -mt-80 left-0 w-full h-44 flex items-center justify-center">
+          <div className="text-white p-4 text-4xl font-bold font-poppins">
+            BOOK A UNIQUE EXPERIENCE ON BALI CEMERLANG TOURS TODAY
+          </div>
+        </div>
         <div class='-mt-14'>
           <SearchCity/>
           </div>
--- src/view/landingPage/LandingPage.js
+++ src/view/landingPage/LandingPage.js
@@ -15,13 +15,6 @@ export default function LandingPage() {
     <div className="h-screen overflow-y-visible">
       <div className="max-w-screen mx-auto">
         <img src='/asset/background.png' alt='background' className='w-full bg-cover'/>
-          <div>
-            <div className="absolute top-[700px] left-0 w-full h-44 flex items-center justify-center">
-              <div className="text-white p-4 text-4xl font-bold font-poppins">
-                BOOK A UNIQUE EXPERIENCE ON BALI CEMERLANG TOURS TODAY
-              </div>
-            </div>
-          </div>
       </div>
       <Navbar/>
       <TourMenu/>
