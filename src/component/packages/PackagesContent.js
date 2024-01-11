3cb265bf6088cc7c285b6a06e7b0a20dc8f9de93
Angga Aditya <theshadow1325@gmail.com>
2024-01-11 12:00:36 +0800

    add paketlist

--- src/component/packages/PackagesContent.js
+++ src/component/packages/PackagesContent.js
@@ -1,19 +1,34 @@
 import React from 'react'
-import VisaPackages from './VisaPackages'
-import RentalSilver from './RentalSilver'
-import RentalGold from './RentalGold'
-import RentalPlatinum from './RentalPlatinum'
+import { Link } from 'react-router-dom'
+import SearchPackage from '../Searchpackage/SearchPackage'
+
 
 export default function PackagesContent() {
   return (
     <div class='pt-44'>
-        <div class='text-center font-bold text-5xl'>Packges</div>
+      <Link to='/'>
+        <img src='./asset/back.svg' alt='back' className='pl-[120px]'/>
+      </Link>
+      <div className='flex justify-center'>
+        <img src='asset/icon-park-solid_ferris-wheel.png' alt='tour' className='pr-6'/>
+        <div class='font-bold text-5xl text-VividRed'>Packages</div>
+      </div>
+      <div className='flex justify-center pt-8'>
+        <div className='flex items-center justify-center w-[1500px] h-40 bg-LightGrayishViolet rounded-xl'>
+          <SearchPackage/>
+        </div>
+      </div>
+      <div class='flex justify-center item-center -mt-7'>
+        <button class='h-14 w-[640px] bg-VividRed rounded-xl font-poppins text-xl text-white'>
+          SEARCH
+        </button>
+      </div>
+      
+        
+        
         <div class='pt-16 pb-36'>
             <div class='flex flex-row px-28'>
-                <div class='basis-1/4'><VisaPackages/></div>
-                <div class='basis-1/4'><RentalSilver/></div>
-                <div class='basis-1/4'><RentalGold/></div>
-                <div class='basis-1/4'><RentalPlatinum/></div>
+                
             </div>
         </div>
         
